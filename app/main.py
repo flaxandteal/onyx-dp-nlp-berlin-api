e5aa48900f0cd175d7b9c66286b91a81fa22b105
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2023-07-05 12:33:08 +0100

    Add startup config logs

--- app/main.py
+++ app/main.py
@@ -1,9 +1,22 @@
 from flask import Flask
+import logging
+import sys
 
 from app.views.berlin import berlin_blueprint
 from app.views.health import health_blueprint
-from app.settings import settings
+from app.settings import settings, get_custom_settings
+from app.logger import configure_logging, setup_logger
 
+logging.basicConfig(
+    format="%(message)s",
+    stream=sys.stdout,
+    level=logging.INFO,
+)
+
+configure_logging()
+logger = setup_logger()
+
+logger.info("initial configuration", data=get_custom_settings())
 
 def create_app():
     application = Flask(__name__)
--- app/settings.py
+++ app/settings.py
@@ -6,7 +6,7 @@ current_time = int(time.time())
 settings = Dynaconf(
     envvar_prefix="BERLIN_API",
     load_dotenv=True,
-    dotenv_path=".env.default",
+    dotenv_path=".env.local",
 )
 
 settings.reload()
@@ -17,3 +17,11 @@ settings.NAMESPACE = settings.get("LOGGING_NAMESPACE")
 settings.DATA_LOCATION = settings.get("DATA_LOCATION")
 settings.BUILD_TIME = settings.get("BUILD_TIME", current_time)
 settings.GIT_COMMIT = settings.get("GIT_COMMIT")
+
+def get_custom_settings():
+    custom_settings = {}
+    vars = ["HOST", "PORT", "NAMESPACE", "DATA_LOCATION", "BUILD_TIME", "GIT_COMMIT"]
+    for v in vars:
+        custom_settings[v] = settings[v]
+    
+    return custom_settings
